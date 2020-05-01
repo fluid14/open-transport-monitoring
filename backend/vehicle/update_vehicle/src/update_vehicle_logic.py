650af14e758bbb6d6b7bb57b9e4d3f74452b0614
Filip Baumgart <f.m.baumgart@hotmail.com>
2020-05-01 15:07:36 +0000

    Merged in OTM-51_fix_updateVehicle (pull request #18)
    OTM-51 Fix updateVehicle logic to return object
    
    * OTM-51 Fix updateVehicle logic to return object
    
    Approved-by: Dawid Czarczyński

--- backend/vehicle/update_vehicle/src/update_vehicle_logic.py
+++ backend/vehicle/update_vehicle/src/update_vehicle_logic.py
@@ -7,14 +7,13 @@ logger.setLevel(logging.INFO)
 
 
 def update_vehicle_logic(data, storage):
-    query = "update vehicles set NumberPlate=%s, Brand=%s, Model=%s, DeviceID=%s where VehicleID=%s"
-    result = storage.execute(query, data)
-    if result:
-        storage.connect()
-        query = "select * from vehicles where VehicleID=%s"
-        vehicle_id = data[3]
-        params = (vehicle_id,)
-        result = storage.select(query, params)
-        return result
+    update_query = "update vehicles set NumberPlate=%s, Brand=%s, Model=%s, DeviceID=%s where VehicleID=%s"
+    update_result = storage.execute(update_query, data)
+    select_query = "select * from vehicles where VehicleID=%s"
+    vehicle_id = data[4]
+    params = (vehicle_id,)
+    select_result = storage.select(select_query, params)
+    if select_result:
+        return select_result
     else:
         raise VehicleNotFound
